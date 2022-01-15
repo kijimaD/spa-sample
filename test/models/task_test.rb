9ed7030e528a2d8d28d718f1d323966b5aeb59bb
kijima <norimaking777@gmail.com>
2022-01-15 15:52:27 +0900

    chore: run annotate

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id         :integer          not null, primary key
+#  is_done    :boolean
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class TaskTest < ActiveSupport::TestCase
