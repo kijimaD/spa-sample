a6c14dece640cc9cc428243f4083308429b949d8
kijima <norimaking777@gmail.com>
2022-01-15 15:27:51 +0900

    feat: task controller response

--- Makefile
+++ Makefile
@@ -2,3 +2,9 @@ shell:
 	sudo docker-compose run -u "$(id -u $USER):$(id -g $USER)" rails /bin/sh
 reset:
 	sudo chown -R $USER:$USER .
+up:
+	docker-compose up -d
+seed:
+	rails db:seed
+reset
+	rails db:migrate:reset
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: '仕事', is_done: true)
+Task.create(name: '掃除', is_done: false)
